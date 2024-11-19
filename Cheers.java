c1e3c2a1b2ef99e024997d194411beac8eece67b
TalGrinberg20 <talgrinberg20@gmail.com>
2024-11-19 18:24:47 +0200

    test

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,20 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        String word = args[0].toUpperCase();
+        int repeat = Integer.parseInt(args[1]);
+        String differentLetters = "AEFHILMNORSX";
+         for (int i = 0; i < word.length(); i++) {
+             if(differentLetters.indexOf(word.charAt(i)) == -1){
+                System.out.println("Give me a  " + word.charAt(i) + ":" + word.charAt(i)+ "!");
+             }
+             else {
+                System.out.println("Give me an " + word.charAt(i) + ":" + word.charAt(i)+ "!"); 
+             }
+         }
+         System.out.println("What does that spell?");
+         for (int i = 0; i < repeat; i++) {
+                System.out.println(word + "!!!");
+         }
         }
 }
