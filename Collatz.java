5ea74face95f9deb90bba9ab1d0ea5b9dca5d668
TalGrinberg20 <talgrinberg20@gmail.com>
2024-11-21 09:27:18 +0200

    test

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,25 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int seed = Integer.parseInt(args [0]);
+		int currentNum = 0;
+		int firstNum = 1;
+		String mode = args[1];
+	for (int i = 0; i <seed; i++) {
+		System.out.print(firstNum + " ");
+			while (currentNum != 1) { 
+			if (currentNum%2 == 0){
+				currentNum = currentNum/2;
+				}
+			else {
+				currentNum = currentNum*3 +1 ;
+				}
+			
+		System.out.print(currentNum + " ");
+			}
+		}
 	}
 }
+
+			
+
